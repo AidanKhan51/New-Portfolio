947c4984965c87f856f58f6583c0250ecaf264fb
Aidan Khan <aidan.khan31@gmail.com>
2025-08-26 14:56:54 -0400

    Updates

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,23 @@
 
 "use strict";
 
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('proj1').addEventListener('click', () => {
+        console.log("sus")
+        openWindow();
+    });
+
+})
+
+function openWindow() {
+    const popup = document.getElementById('popup')
+    popup.showModal();
+    document.documentElement.classList.add("modal-open")
+
+
+    document.getElementById('close').addEventListener('click', () => {
+        console.log("baka")
+        popup.close();
+        document.documentElement.classList.remove("modal-open")
+    });
+}
