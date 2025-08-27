6f2c04f7ff3e7ab8ad0706919bf6f4a431794b00
Aidan Khan <aidan.khan31@gmail.com>
2025-08-26 23:31:52 -0400

    Updating too much to properly commit lol

--- JS/script.js
+++ JS/script.js
@@ -7,21 +7,21 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('proj1').addEventListener('click', () => {
-        console.log("sus")
         openWindow();
     });
-
 })
 
 function openWindow() {
     const popup = document.getElementById('popup')
     popup.showModal();
-    document.documentElement.classList.add("modal-open")
+    popup.classList.add("modal-open")
 
 
     document.getElementById('close').addEventListener('click', () => {
-        console.log("baka")
-        popup.close();
-        document.documentElement.classList.remove("modal-open")
+        popup.classList.remove("modal-open")
+        setTimeout(() => {
+            popup.close();
+        }, "500");
     });
 }
+
